a1b3bac15cce6e2dd99fb95d572adf4255425d62
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2021-05-25 13:16:04 -0600

    add contact popup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function ShowContact(){
+    document.getElementById("contact-form").style.display = "block";
+    document.getElementById("contact-button").style.display = "none";
+}
+
+function CloseContact(){
+    document.getElementById("contact-form").style.display = "none";
+    document.getElementById("contact-button").style.display = "block";
+}
