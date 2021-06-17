5f5e574297e1c1b60d071249251cfe18eb5e8cb1
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2021-06-17 14:09:16 -0600

    add stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 function ShowContact(){
     document.getElementById("contact-form").style.display = "block";
     document.getElementById("contact-button").style.display = "none";
@@ -7,3 +9,10 @@ function CloseContact(){
     document.getElementById("contact-form").style.display = "none";
     document.getElementById("contact-button").style.display = "block";
 }
+
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
